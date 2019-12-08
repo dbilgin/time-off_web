40c152494e6e3952615e0c45031e9e738cef5570
Deniz Bilgin <denizcanbilgin@gmail.com>
2019-12-08 02:53:41 +0100

    theme setting

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,13 +6,7 @@
         : $vuetify.theme.themes.light.background
     }"
   >
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-navigation-drawer :mini-variant="drawer" fixed app>
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
@@ -30,17 +24,8 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn @click.stop="miniVariant = !miniVariant" icon>
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn @click.stop="clipped = !clipped" icon>
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn @click.stop="fixed = !fixed" icon>
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-select
@@ -50,29 +35,13 @@
         label="Select Theme"
         solo
       ></v-select>
-      <v-spacer />
-      <v-btn @click.stop="rightDrawer = !rightDrawer" icon>
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
+    <v-footer :fixed="false" app>
       <span>&copy; 2019</span>
     </v-footer>
   </v-app>
@@ -83,17 +52,16 @@ export default {
   data() {
     return {
       selectedTheme: null,
-      clipped: false,
-      drawer: false,
-      fixed: false,
+      drawer: true,
       themes: [
         { text: 'Light', value: 'light' },
         { text: 'Dark', value: 'dark' }
       ],
+      title: 'Time-Off',
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Home',
           to: '/'
         },
         {
@@ -101,15 +69,16 @@ export default {
           title: 'Inspire',
           to: '/inspire'
         }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      ]
     }
   },
   mounted() {
-    this.selectedTheme = localStorage.getItem('theme')
+    let theme = localStorage.getItem('theme')
+    if (!theme) {
+      theme = 'light'
+      localStorage.setItem('theme', theme)
+    }
+    this.selectedTheme = theme
   },
   methods: {
     themeSelected(value) {
